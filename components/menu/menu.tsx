9cabd9b57e25d9d93c445119ba5836eb46c093ca
ygzaydn <ygzaydns@gmail.com>
2022-09-12 09:30:15 +0300

    added event listener for menu

--- components/menu/menu.tsx
+++ components/menu/menu.tsx
@@ -1,19 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Link from "next/link";
 
 const Menu = () => {
     const [toggle, setToggle] = useState<boolean>(false);
+
+    useEffect(() => {
+        const element = document.getElementById("__next");
+        element?.addEventListener("click", (event) => {
+            const target = event.target as HTMLElement;
+            if (!target.className.includes("menu")) {
+                setToggle(false);
+            }
+        });
+    }, []);
 
     return (
         <nav className={toggle ? "menu menu--shift" : "menu"}>
             <img
                 src="/logos/menu.svg"
                 alt="menu-icon"
+                className="menu--image"
                 onClick={() => setToggle(!toggle)}
             />
             <ul className="menu--container">
-                <li>Home</li>
-                <li>Giveaways</li>
-                <li>News</li>
+                <Link href="/">Home</Link>
+                <Link href="/giveaways">Giveaways</Link>
+                <Link href="/news">News</Link>
             </ul>
         </nav>
     );
